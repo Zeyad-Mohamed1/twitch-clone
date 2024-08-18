ae82ae303356cc852b14edd23acee052a7b01384
zeyad mohamed <zeyadelsharawy709@gmail.com>
2024-08-18 20:31:09 +0300

    Implement Recommended Section in Sidebar

---
+++ lib/auth-service.ts
@@ -0,0 +1,22 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+
+export const getSelf = async () => {
+  const user = await currentUser();
+
+  if (!user || !user.username) {
+    throw new Error("Unauthorized");
+  }
+
+  const self = await db.user.findUnique({
+    where: {
+      externalUserId: user.id,
+    },
+  });
+
+  if (!self) {
+    throw new Error("No user found");
+  }
+
+  return self;
+};
