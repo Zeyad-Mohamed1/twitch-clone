22488932732da2003ac288a56592299836df0038
zeyad mohamed <zeyadelsharawy709@gmail.com>
2024-08-29 22:36:19 +0300

    Complete Block System

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -2,6 +2,7 @@ import { isFollowingUser } from "@/lib/follow-service";
 import { getUserByUsername } from "@/lib/user-service";
 import { notFound } from "next/navigation";
 import { Actions } from "./_components/actions";
+import { isBlockedByUser } from "@/lib/block-service";
 
 interface UserpageProps {
   params: {
@@ -12,16 +13,17 @@ const Userpage = async ({ params }: UserpageProps) => {
   const user = await getUserByUsername(params.username);
 
   if (!user) {
-    return notFound();
+    notFound();
   }
 
   const isFollowing = await isFollowingUser(user.id);
-
+  const isBlocked = await isBlockedByUser(user.id);
   return (
     <div className="flex flex-col gap-y-4">
       <p>username : {user.username}</p>
       <p>id : {user.id}</p>
       <p>isFollowing : {isFollowing.toString()}</p>
+      <p>isBlocked : {isBlocked.toString()}</p>
       <Actions userId={user?.id} isFollowing={isFollowing} />
     </div>
   );
